22b708ba964a799d72392b7aac1ac9266a06f3bd
Aidan Follestad <afollestad@squareup.com>
2018-12-03 13:02:14 -0800

    Update license header

--- spotless.license.kt
+++ spotless.license.kt
@@ -13,14 +13,3 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.afollestad.viewpagerdots
-
-import android.graphics.drawable.Drawable
-import androidx.annotation.ColorInt
-import androidx.core.graphics.drawable.DrawableCompat
-
-internal fun Drawable.tint(@ColorInt color: Int): Drawable {
-  val wrapped = DrawableCompat.wrap(this)
-  DrawableCompat.setTint(wrapped, color)
-  return wrapped
-}
